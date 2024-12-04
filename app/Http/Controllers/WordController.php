bd537cefd20897d700dbd86d66461eb92fb1092f
UTYNM <anumuti021@gmail.com>
2024-12-04 09:01:29 +0700

    "Added WordController and updated routes in web.php"

---
+++ app/Http/Controllers/WordController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Operator;
+use Illuminate\Http\Request;
+
+class WordController extends Controller
+{
+}
--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,8 @@ Route::get('/', function () {
 Route::get('/youtube', [PostYoutubeController::class, 'index'])->name('youtube.index');
 Route::get('/youtube/authenticate', [PostYoutubeController::class, 'authenticate'])->name('youtube.authenticate');
 Route::get('/youtube/callback', [PostYoutubeController::class, 'callback'])->name('youtube.callback');
-Route::post('/youtube/upload', [PostYoutubeController:: class, 'upload'])->name('youtube.upload');
-Route::post('/youtube/upload', [PostYoutubeController:: class, 'upload'])->name('youtube.upload');
+Route::post('/youtube/upload', [PostYoutubeController::class, 'upload'])->name('youtube.upload');
+Route::post('/youtube/upload', [PostYoutubeController::class, 'upload'])->name('youtube.upload');
 
 //twitter
 // Route::get('/post-tweet', [PostTwitterController::class, 'postTweet']);
@@ -27,12 +27,11 @@ Route::get('/twitter', [PostTwitterController::class, 'index'])->name('twitter.i
 Route::post('/post-tweet', [PostTwitterController::class, 'postTweet']);
 
 Route::controller(WordController::class)->group(function () {
-    Route::get('/word', 'index');
-    Route::get('/word/export', 'export');
+    Route::get('/', 'index');
 });
 
 Route::controller(OperatorController::class)->group(function () {
-    Route::get('/operator', 'index');;
+    Route::get('/operator', 'index');
     Route::get('/operator/create', 'create');
     Route::post('/operator/store', 'store');
     Route::get('/operator/{operator}/edit', 'edit');
@@ -43,5 +42,4 @@ Route::controller(OperatorController::class)->group(function () {
 Route::controller(JsonController::class)->group(function () {
     Route::get('map', 'index');
     Route::post('create', 'create');
-
 });
